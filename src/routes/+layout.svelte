eb0abff7c4b598d65a572fdbd947cd35dc8605fd
Christian Clark <christianclark687@gmail.com>
2024-06-16 19:19:49 -0400

    Initial basic site from svelte template

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import logo from '$lib/ateam-ball-no-background.png';
+</script>
+
+<nav>
+	<a href="/robots">Robots</a>
+	<a href="/open-source">Open Source</a>
+    <a href="/">
+        <img src={logo} alt="The A-Team Logo">
+    </a>
+	<a href="/blog">Blog</a>
+	<a href="/events">Events</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="The A-Team Website" />
+</svelte:head>
+
+<h1>The A-Team</h1>
+<h2>RoboCup SSL</h2>
