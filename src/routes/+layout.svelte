4a1417e6a23dadb5c7681619d7dd168638bfd18f
Christian Clark <christianclark687@gmail.com>
2024-07-06 21:16:45 -0400

    Front page stylez

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,14 @@
     import "../app.css";
 </script>
 
-<nav class="h-24 bg-lime-400 flex w-full justify-between">
-	<a href="/robots" class="flex-auto my-8 basis-1/5 text-center">Robots</a>
-	<a href="/open-source" class="flex-auto my-8 basis-1/5 text-center">Open Source</a>
-    <a href="/" class="flex-auto my-2 basis-1/5 flex justify-center">
+<nav class="h-24 flex w-full justify-between">
+	<a href="/robots" class="flex-auto my-8 basis-1/5 text-center hover:text-yellow-500">Robots</a>
+	<a href="/open-source" class="flex-auto my-8 basis-1/5 text-center hover:text-yellow-500">Open Source</a>
+    <a href="/" class="flex-auto my-2 basis-1/5 flex justify-center hover:text-yellow-500">
         <img src={logo} alt="The A-Team Logo" class="h-20 mx-0">
     </a>
-	<a href="/blog" class="flex-auto my-8 basis-1/5 text-center">Blog</a>
-	<a href="/events" class="flex-auto my-8 basis-1/5 text-center">Events</a>
+	<a href="/blog" class="flex-auto my-8 basis-1/5 text-center hover:text-yellow-500">Blog</a>
+	<a href="/events" class="flex-auto my-8 basis-1/5 text-center hover:text-yellow-500">Events</a>
 </nav>
 
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,12 @@
 	<meta name="description" content="The A-Team Website" />
 </svelte:head>
 
-<h1>The A-Team</h1>
-<h2>RoboCup SSL</h2>
-
-<img src={league} alt="RoboCup SSL League 2023"/>
+<div class="w-full">
+	<div>
+		<h1 class="text-center my-2 text-2xl">The A-Team</h1>
+		<h2 class="text-center text-xl">RoboCup SSL</h2>
+	</div>
+	<div class="flex justify-center">
+		<img src={league} alt="RoboCup SSL League 2023" class="size-3/5 flex-none my-8 rounded-md border-4 border-yellow-500"/>
+	</div>
+</div>
